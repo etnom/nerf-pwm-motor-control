8ac329095cc6bfaa15627eb48fd203672d56a183
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-02 19:02:27 -0700

    add comments

--- nerf-pwm-motor-control.ino
+++ nerf-pwm-motor-control.ino
@@ -1,10 +1,10 @@
-#define POT_IN_PIN 0		
-#define PWM_OUT_PIN 3		
+#define POT_IN_PIN 0		//pin for poteniometer as input
+#define PWM_OUT_PIN 3		//pin for pwm
 
 void setup () {
-	pinMode(PWM_OUT_PIN, OUTPUT);		
+	pinMode(PWM_OUT_PIN, OUTPUT);		//set pwm pin to output
 }
 
 void loop () {
-	analogWrite(PWM_OUT_PIN, analogRead(POT_IN_PIN)/4);		
+	analogWrite(PWM_OUT_PIN, analogRead(POT_IN_PIN)/4);		//write PWM depending on pot value
 }
