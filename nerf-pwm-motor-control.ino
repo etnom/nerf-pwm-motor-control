1877b2c35f2538cafc45f90a09affa254e8a1d35
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-02 19:12:52 -0700

    change pwm pin, remove pinMode

--- nerf-pwm-motor-control.ino
+++ nerf-pwm-motor-control.ino
@@ -1,8 +1,7 @@
 #define POT_IN_PIN 0		//pin for poteniometer as input
-#define PWM_OUT_PIN 3		//pin for pwm
+#define PWM_OUT_PIN 5		//pin for pwm, using pin 5 for 980 Hz PWM
 
 void setup () {
-	pinMode(PWM_OUT_PIN, OUTPUT);		//set pwm pin to output
 }
 
 void loop () {
