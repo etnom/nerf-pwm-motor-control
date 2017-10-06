9a8bcaa263ecb0db728a7018ac9399aff91eac74
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-06 16:41:36 -0700

    motor at full pwm to reach acceleration when trigger first pressed

--- nerf-pwm-motor-control.ino
+++ nerf-pwm-motor-control.ino
@@ -6,12 +6,22 @@
 
 Button triggerBtn (TRIGGER_PIN, true, true, 25);		//button class, invert and internal pullups with 20 ms debounce time
 
+boolean hasAccelerated = false;		//boolean to check if motor spinning at full speed
+
 void setup() {
 
 }
 
 void loop () {
-	if (triggerBtn.isPressed()) {		//if trigger pressed
-		analogWrite(PWM_OUT_PIN, analogRead(POT_IN_PIN)/4);		//write PWM depending on pot value
+	if(triggerBtn.wasPressed()) {
+		digitalWrite(PWM_OUT_PIN, HIGH);
+		delay(100);		//time for motor to accelerate to full speed, may need to change depending on motor torque
+		hasAccelerated = true;
+	} else if (triggerBtn.wasReleased()) {
+		hasAccelerated = false;				
 	}
+
+	if (triggerBtn.isPressed() && hasAccelerated) {		//if trigger pressed
+		analogWrite(PWM_OUT_PIN, analogRead(POT_IN_PIN)/4);		//write PWM depending on pot value
+	} 
 }
