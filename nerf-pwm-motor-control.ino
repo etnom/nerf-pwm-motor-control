8622a09fbe9823ef7bd4089dd7f0f6edb84e5476
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-06 16:43:21 -0700

    macro motor accel val

--- nerf-pwm-motor-control.ino
+++ nerf-pwm-motor-control.ino
@@ -1,8 +1,9 @@
 #include <Button.h>			//library for dealing with buttons
 
-#define POT_IN_PIN 0		//pin for poteniometer as input
-#define PWM_OUT_PIN 5		//pin for pwm, using pin 5 for 980 Hz PWM
-#define TRIGGER_PIN 4		//pin for trigger
+#define POT_IN_PIN 0			//pin for poteniometer as input
+#define PWM_OUT_PIN 5			//pin for pwm, using pin 5 for 980 Hz PWM
+#define TRIGGER_PIN 4			//pin for trigger
+#define MOTOR_ACCEL_TIME 200	//time to allow motor to reach full speed, in ms. May need to be adjusted depending on motor torque
 
 Button triggerBtn (TRIGGER_PIN, true, true, 25);		//button class, invert and internal pullups with 20 ms debounce time
 
@@ -15,7 +16,7 @@ void setup() {
 void loop () {
 	if(triggerBtn.wasPressed()) {
 		digitalWrite(PWM_OUT_PIN, HIGH);
-		delay(100);		//time for motor to accelerate to full speed, may need to change depending on motor torque
+		delay(MOTOR_ACCEL_TIME);			//allow motor to reach full speed
 		hasAccelerated = true;
 	} else if (triggerBtn.wasReleased()) {
 		hasAccelerated = false;				
